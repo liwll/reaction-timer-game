0eadaf503a2ff9feef95088b88dee99519f1a88c
liwll <liwill7778@gmail.com>
2021-07-02 13:54:31 -0400

    Created basic working game components, will update the UI next

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @stop="showResults" />
+  <Results v-if="donePlaying" :reactionTime="reactionTime"/>
+</template>
+
+<script>
+import Block from './components/Block.vue';
+import Results from './components/Results.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Block,
+    Results,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      reactionTime: null,
+      donePlaying: false,
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 3000 + Math.random() * 7000;
+      this.isPlaying = true;
+      this.donePlaying = false;
+    },
+    showResults(reactionTime) {
+      this.isPlaying = false;
+      this.reactionTime = reactionTime;
+      this.donePlaying = true;
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,12 @@
+<template> 
+    <p>{{ reactionTime }} ms</p> 
+</template>
+
+<script>
+
+export default {
+    props: ['reactionTime'],
+
+}
+
+</script>
